f010856e1a6f8a7d17e5d61853778efa85e538b5
Clarence21-ai <agtualclarence16@gmail.com>
2024-09-11 11:28:07 +0000

    initial commit with app.js

---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = "Clarence";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male"
+let twitterHandle = "Calrence Ja"
+
+console.log("Name:", name);
+console.log("What do you want to become:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
