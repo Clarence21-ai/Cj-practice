e000abab27b218978fcb0a7fce8ca99d77bd6e49
Clarence21-ai <agtualclarence16@gmail.com>
2024-09-11 11:49:46 +0000

    initial commit with app.js

--- numbers.js
+++ numbers.js
@@ -18,3 +18,21 @@ console.log("Modulus:", modulusResult);
 
 let exponentiationResult = firstFavNum ** secondFavNum;
 console.log("Exponentiation (Power):", exponentiationResult);
+
+let additionResult = firstFavNum + secondFavNum;
+console.log("Addition:", additionResult);
+
+let subtractionResult = firstFavNum - secondFavNum;
+console.log("Subtraction:", subtractionResult);
+
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log("Multiplication:", multiplicationResult);
+
+let divisionResult = firstFavNum / secondFavNum;
+console.log("Division:", divisionResult);
+
+let modulusResult = firstFavNum % secondFavNum;
+console.log("Modulus:", modulusResult);
+
+let exponentiationResult = firstFavNum ** secondFavNum;
+console.log("Exponentiation (Power):", exponentiationResult);
