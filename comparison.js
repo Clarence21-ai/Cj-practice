949aa722eb7f0d222e28963e84ca2f1aabaf556e
Clarence21-ai <agtualclarence16@gmail.com>
2024-09-12 06:52:20 +0000

    initial commit with app.js

---
+++ comparison.js
@@ -0,0 +1,14 @@
+let firstFavNumb = 21; 
+let secondFavNumb = 3; 
+
+console.log(firstFavNumb > secondFavNumb);     
+console.log(firstFavNumb < secondFavNumb);     
+console.log(firstFavNumb >= secondFavNumb);    
+console.log(firstFavNumb <= secondFavNumb);    
+console.log(firstFavNumb === secondFavNumb);  
+console.log(firstFavNumb == secondFavNumb);    
+console.log(firstFavNumb !== secondFavNumb);  
+console.log(firstFavNumb != secondFavNumb);    
+console.log(firstFavNumb > 0 && secondFavNumb > 0);  
+console.log(firstFavNumb > 10 || secondFavNumb > 10); 
+console.log(!(firstFavNumb > secondFavNumb));  
