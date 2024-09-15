bf5c357b809cbb539f7d67e51879ca0f27781b80
Clarence21-ai <agtualclarence16@gmail.com>
2024-09-15 02:04:39 +0000

    initial commit with app.js

---
+++ string.js
@@ -0,0 +1,19 @@
+let favActorFirstName = "Robin";
+let favActorLastName = "Padilla";
+
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+
+let lowercase = "shadow".charAt(0).toUpperCase() + "shadow".slice(1);
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += " and his best show is Silicon Valley.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message
